0cbd32c27fedc62c89cb399a62716931cd56ec8a
Jonathan Bral <bral.pdx@gmail.com>
2020-02-16 15:36:58 -0800

    added two resource files. One for service data, and another for user data.

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!\n";
+	std::cout << "=======================\n"; 
+	std::cout << "== Welcome to ChocAn ==\n";
+	std::cout << "=======================\n";
 	std::cin.get();
 	return RETURN_SUCCESS;
 }
