abd001ce67d4bd78d025b119073f6090956522a2
Jonathan Bral <bral.pdx@gmail.com>
2020-03-13 17:39:46 -0700

    Added testing files

---
+++ Source/tests/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../../utilities.h"
+#include "../../bst.h"
+#include "../../user_service_obj.h"
+
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(sign_in_test)
+		{
+
+		}
+	};
+}
